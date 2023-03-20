8fd658024d0a90466cb74fc83e2eee90c8e90903
GabrielTulao <125254707+GabrielTulao@users.noreply.github.com>
2023-03-20 18:29:30 +0800

    Create indexActivity.js

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = "Gabriel";
+let lastName = "Tulao";
+let age = 20;
+let hobbies = ['Video Games', 'Listening to music'];
+let homeAddress = {
+	houseNumber: "220",
+	street: "Bisita",
+	subdivision: "N/A",
+	barangay: "Balsahan",
+	city: "Kawit",
+	province: "Cavite",
+	zipCode: 4104
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
